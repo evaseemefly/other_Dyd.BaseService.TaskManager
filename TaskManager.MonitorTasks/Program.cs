d15ac5ff297748b5cdd54bd70401f7ed973caf78
chejiangyi <472790378@qq.com>
2016-05-30 19:37:52 +0800

    提交

---
+++ TaskManager.MonitorTasks/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManager.MonitorTasks
+{
+    public class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            TaskManageErrorSendTask task = new TaskManageErrorSendTask();
+            task.SystemRuntimeInfo = new BSF.BaseService.TaskManager.SystemRuntime.TaskSystemRuntimeInfo() { TaskConnectString = "server=192.168.17.201;Initial Catalog=dyd_bs_task;User ID=sa;Password=Xx~!@#;" };
+
+            task.TestRun();
+        }
+    }
+}
